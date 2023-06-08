f5b080c23467ff5c8a4df10a6b30c2f399b8149e
weiyunbo <820437801@qq.com>
2023-06-08 15:13:26 +0800

    画流程

---
+++ demo-activiti01/src/main/java/com/wei/demo_activiti01/DemoActiviti01Application.java
@@ -0,0 +1,11 @@
+package com.wei.demo_activiti01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoActiviti01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoActiviti01Application.class, args);
+    }
+}
